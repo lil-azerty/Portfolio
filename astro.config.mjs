676bf5d0856ddbde7d782d1e8c1b3d405bd54ebf
lil-azerty <alandelapro@gmail.com>
2024-10-04 10:45:21 +0200

    changement slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lil-azerty.github.io",
-  base: "Portfolio",
+  base: "/Portfolio",
 });
