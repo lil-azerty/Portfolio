38e5c91a1bec25b055110aa452114031e2943de6
lil-azerty <alandelapro@gmail.com>
2024-10-04 09:10:32 +0200

    enlever base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lil-azerty.github.io",
-  base: "Portfolio",
   // Premier commit
   // deuxieme commit
 });
